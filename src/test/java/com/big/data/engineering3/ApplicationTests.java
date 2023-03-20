199bea0fe3acd46c9a06404d0bc01c33a0b7e2c0
desmond <sohskd@hotmail.com>
2023-03-20 09:36:19 +0800

    first commit

---
+++ src/test/java/com/big/data/engineering3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.big.data.engineering3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
